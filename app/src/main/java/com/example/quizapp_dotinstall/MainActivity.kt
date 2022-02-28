15fd4f147121f4e9c4047dea2057e6dbfa663480
Hamaya-Mitsuharu <NishinNishin0@gmail.com>
2022-02-28 22:30:05 +0900

    add loadQuizSet() and add tab character in quiz.txt

--- app/src/main/java/com/example/quizapp_dotinstall/MainActivity.kt
+++ app/src/main/java/com/example/quizapp_dotinstall/MainActivity.kt
@@ -2,14 +2,38 @@ package com.example.quizapp_dotinstall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.*
 
 class MainActivity : AppCompatActivity() {
-    var quizSet = arrayOf(0)
+    var quizSet = arrayOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loadQuizSet();
+        loadQuizSet()
+    }
+
+    private fun loadQuizSet(){
+        try {
+            val file = resources.assets.open("quiz.txt")
+            val bufferedReader = BufferedReader(InputStreamReader(file))
+
+            // １行ずつ読み込む
+            var index = 0
+            var line = bufferedReader.readLine()
+            while (line != null){
+                Log.d("readLineInQuizTxt", line)
+                quizSet += line.split("\t")
+                line = bufferedReader.readLine()
+            }
+            for (s in quizSet){
+                Log.d("readLineInQuizTxt", s)
+            }
+            // kotlinでは内部的にuse()を用いているためclose()は暗黙的に実行される
+        } catch (e: FileNotFoundException){
+            Log.e("FileNotFoundexception", e.toString())
+        }
     }
 }
