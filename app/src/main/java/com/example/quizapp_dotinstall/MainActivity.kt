58420e5eb3bab2c7d83a8e8e9a1529cf33439c9f
Hamaya-Mitsuharu <NishinNishin0@gmail.com>
2022-03-02 17:59:21 +0900

    add getview() in MainActivity.kt

--- app/src/main/java/com/example/quizapp_dotinstall/MainActivity.kt
+++ app/src/main/java/com/example/quizapp_dotinstall/MainActivity.kt
@@ -3,17 +3,41 @@ package com.example.quizapp_dotinstall
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.example.quizapp_dotinstall.databinding.ActivityMainBinding
 import java.io.*
 
 class MainActivity : AppCompatActivity() {
     // 問題文と単語が交互に入っている
     var quizSet = arrayOf<String>()
 
+    lateinit var scoreText : TextView
+    lateinit var qText : TextView
+    lateinit var a0Button : Button
+    lateinit var a1Button : Button
+    lateinit var a2Button : Button
+    lateinit var nextButton : Button
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         loadQuizSet()
+        getViews()
+    }
+
+    private fun getViews(){
+        scoreText = binding.scoreText
+        qText = binding.qText
+        a0Button = binding.a0Button
+        a1Button = binding.a1Button
+        a2Button = binding.a2Button
+        nextButton = binding.nextButton
     }
 
     private fun loadQuizSet(){
@@ -25,12 +49,8 @@ class MainActivity : AppCompatActivity() {
             var index = 0
             var line = bufferedReader.readLine()
             while (line != null){
-                Log.d("readLineInQuizTxt", line)
                 quizSet += line.split("\t")
                 line = bufferedReader.readLine()
-            }
-            for (s in quizSet){
-                Log.d("readLineInQuizTxt", s)
             }
             // kotlinでは内部的にuse()を用いているためclose()は暗黙的に実行される
         } catch (e: FileNotFoundException){
